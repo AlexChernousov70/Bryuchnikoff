f8430fca1ca3c0d80552f464bfd106181d1782a7
Alex <alex70.07@mail.ru>
2025-07-27 15:40:27 +0300

    Настраиваем чтобы не кешировалась статика при разработке

--- Bryuchnikoff/settings.py
+++ Bryuchnikoff/settings.py
@@ -117,3 +117,10 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+if DEBUG:
+    import mimetypes
+    mimetypes.add_type("application/javascript", ".js", True)
+    MIDDLEWARE += ('django.middleware.common.CommonMiddleware',)
+
+    
