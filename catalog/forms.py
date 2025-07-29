48576120976e3f338768469c986e49b4345dbae6
Alex <alex70.07@mail.ru>
2025-07-29 22:52:14 +0300

    Решение проблемы с вечной перезагрузкой капчи

--- catalog/forms.py
+++ catalog/forms.py
@@ -4,7 +4,6 @@ from django_recaptcha.fields import ReCaptchaField
 from django_recaptcha.widgets import ReCaptchaV2Checkbox
 
 
-
 class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
