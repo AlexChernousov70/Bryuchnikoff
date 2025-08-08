3ba54c4c2d83d517f0f9e842d7e1745ccc81aa22
Alex <alex70.07@mail.ru>
2025-08-08 18:42:02 +0300

    Полное устранение 405 ошибки при отправке формы

--- catalog/urls.py
+++ catalog/urls.py
@@ -5,7 +5,7 @@ app_name = 'catalog'
 
 urlpatterns = [
     path('order_call/', OrderCallBackCreateView.as_view(), name='order_call'),
+    path('order_create/', OrderCreateView.as_view(), name='order_create'),
     path('<slug:slug>/', ProductListView.as_view(), name='product_list'),
     path('<slug:slug>/<int:product_id>/', ProductListDetail.as_view(), name='product_detail'),
-    path('create_order/', OrderCreateView.as_view(), name='create_order'),
 ]
